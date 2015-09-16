e3fc83fac39da5ba2c8158a2875f130a5c48bc8b
BrendonGeils <brendonjgeils@gmail.com>
2015-09-16 12:55:07 -0500

    booking included

--- booking.php
+++ booking.php
@@ -51,10 +51,15 @@
   <div class="container">
     <div class="section">
 
-      <form action="MAILTO: bgeils@iastate.edu" method="post" enctype="text/plain">
-          <p> Test </p>
-          <textarea name="message"></textarea>
-          <input type="submit">
+      <form action="MAILTO:someone@example.com" method="post" enctype="text/plain">
+        Name:<br>
+        <input type="text" name="name" value="your name"><br>
+        E-mail:<br>
+        <input type="text" name="mail" value="your email"><br>
+        Comment:<br>
+        <input type="text" name="comment" value="your comment" size="50"><br><br>
+        <input type="submit" value="Send">
+        <input type="reset" value="Reset">
       </form>
     
     </div>
